67eca2fe7f74d5c62a93db4c628c417c86bca55c
조민주 <pida6716@gmail.com>
2025-04-29 16:56:29 +0900

    fix: storybook 실행 시 alias 오류 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+import { mergeConfig } from 'vite';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -12,5 +14,14 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../public'],
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+        },
+      },
+    });
+  },
 };
 export default config;
