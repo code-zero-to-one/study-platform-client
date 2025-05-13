e76fb03257d5d280c20b67117d6d136f0ecc6541
조민주 <pida6716@gmail.com>
2025-05-14 00:17:45 +0900

    feat: 배너 추가
    - 현재 임시 이미지 넣어서 진행
    - 이미지가 어떻게 값이 들어오는지에 따라 추후 수정 필요

---
+++ src/widgets/home/banner.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Image from 'next/image';
+import { Carousel } from '@/shared/ui/carousel';
+import ArrowBackIcon from 'public/icons/arrow_back_ios.svg';
+import ArrowForwardIcon from 'public/icons/arrow_forward_ios.svg';
+
+const bannerImages = [
+   '/images/banner.png',
+   '/images/banner.png',
+   '/images/banner.png',
+];
+
+export default function BannerCarousel() {
+   return (
+      <Carousel.Provider className="rounded-150 border border-border-subtle">
+         <Carousel.Viewport>
+            {bannerImages.map((src, i) => (
+               <Carousel.Slide key={i}>
+                  <div className="relative h-[160px] w-full">
+                     <Image
+                        src={src}
+                        alt={`배너 ${i + 1}`}
+                        fill
+                        className="object-contain"
+                     />
+                  </div>
+               </Carousel.Slide>
+            ))}
+         </Carousel.Viewport>
+         <Carousel.Prev icon={<ArrowBackIcon />} />
+         <Carousel.Next icon={<ArrowForwardIcon />} />
+      </Carousel.Provider>
+   );
+}
