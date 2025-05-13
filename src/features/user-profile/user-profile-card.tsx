af2dd2bd3b6ccb7618b0047ff09b5acfb094b2d8
조민주 <pida6716@gmail.com>
2025-05-13 22:25:36 +0900

    feat: 사이드바 유저 프로필 추가

---
+++ src/features/user-profile/user-profile-card.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image';
+import Toggle from '@/shared/ui/toggle/index';
+import AccessTimeIcon from 'public/icons/access_time.svg'
+import AssignmentIcon from 'public/icons/assignment.svg'
+import CodeIcon from 'public/icons/code.svg'
+import DefaultUserIcon from 'public/icons/DefaultUser.svg'
+import SettingIcon from 'public/icons/setting.svg'
+
+interface UserProfileCardProps {
+   name: string;
+   imageUrl: string;
+   matching: boolean;
+   subject: string;
+   time: string;
+   techStacks: string;
+}
+
+export default function UserProfileCard({
+   name,
+   imageUrl,
+   matching,
+   subject,
+   time,
+   techStacks,
+}: UserProfileCardProps) {
+   return (
+      <section className='p-200 rounded-200 border border-border-subtle bg-text-inverse flex flex-col items-start gap-200'>
+         <div className='flex flex-row gap-200 items-center'>
+            <div className="relative w-[64px] h-[64px] shrink-0">
+               {imageUrl ? (
+                  <Image
+                     src={imageUrl}
+                     alt={name}
+                     width={64}
+                     height={64}
+                     className="rounded-full object-cover"
+                  />
+               ) : (
+                  <DefaultUserIcon width={64} height={64} />
+               )}
+
+               <div className="absolute bottom-0 right-0 w-[24px] h-[24px] bg-background-accent-gray-strong rounded-full flex items-center justify-center">
+                  <SettingIcon />
+               </div>
+            </div>
+            <div className='flex flex-col'>
+               <div className='font-designer-18b'>{name}님</div>
+               <div className='flex flex-row gap-100 items-center'>
+                  <span className='font-designer-14r text-gray-800'>스터디 매칭</span>
+                  <Toggle checked={matching} />
+               </div>
+            </div>
+         </div>
+
+         <div className='w-full bg-background-alternative rounded-100 px-200 py-150 flex flex-col gap-200 font-designer-15m'>
+            <div className='flex items-center gap-100'>
+               <AssignmentIcon />
+               <span>{subject}</span>
+            </div>
+            <div className='flex items-center gap-100'>
+               <AccessTimeIcon />
+               <span>{time}</span>
+            </div>
+            <div className='flex items-center gap-100'>
+               <CodeIcon />
+               <span>{techStacks}</span>
+            </div>
+         </div>
+      </section>
+   );
+}
--- src/widgets/home/sidebar.tsx
+++ src/widgets/home/sidebar.tsx
@@ -4,8 +4,16 @@ import UserProfileCard from "@/features/user-profile/user-profile-card";
 
 export default function Sidebar() {
    return (
-      <aside className="flex flex-col gap-6">
-         <UserProfileCard />
+      <aside className="flex flex-col gap-300">
+         <UserProfileCard
+            name="신채호"
+            imageUrl=""
+            matching={true}
+            subject="CS DeepDive"
+            time="오전, 심야"
+            techStacks="HTML/CSS, Git/GitHub"
+         />
+
          <TodoList />
          <Calendar />
       </aside>
