764da78f57f3c5d761824fde4443c0792ffe48af
unknown <dbf6609@naver.com>
2025-05-16 22:26:39 +0900

    refactor : 회원가입/로그인 페이지 랜딩페이지 분리
    소셜로그인 후 redirect 되는 페이지(signup) 를 별도로 분리했습니다

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
+// 소셜 로그인이 실패할 경우 리다이렉트되는 페이지
 import Landing from "@/features/auth/Landing";
 
+// 랜딩페이지에서 로그인 모달이 뜨는 것으로 파악
 export default function LoginPage() {
-  return <Landing />;
+  return <Landing isSignupPage={false} />;
 }
