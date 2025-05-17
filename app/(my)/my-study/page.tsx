eddbb1eb77c5c68d0b70ad0a3847e3871a9b947e
James <junyoungcat@gmail.com>
2025-05-17 19:37:45 +0900

    feat: add my study card

--- app/(my)/my-study/page.tsx
+++ app/(my)/my-study/page.tsx
@@ -1,3 +1,4 @@
+import MyStudyCard from '@/widgets/my-study/my-study-card';
 import Image from 'next/image';
 
 export default function MyStudy() {
@@ -12,30 +13,29 @@ export default function MyStudy() {
         </div>
 
         <div className="flex gap-[var(--spacing-200)]">
-          <div className="flex flex-1 flex-col gap-[var(--spacing-150)] rounded-[var(--radius-100)] bg-[var(--color-background-alternative)] p-[var(--spacing-250)] text-[var(--color-text-default)]">
-            <div className="text-left text-[15px] font-[var(--font-weight-regular)]">
-              총 참여
-            </div>
-            <div className="text-right text-[24px] font-[var(--font-weight-bold)]">
-              173
-            </div>
+          <MyStudyCard title="총 참여" value="173" />
+          <MyStudyCard title="연속 참여" value="8" unit="주" />
+          <MyStudyCard title="완료" value="23" unit="회" />
+        </div>
+
+        <div className="flex gap-[var(--spacing-200)]">
+          <MyStudyCard title="최대 연속 참여" value="8" unit="주" />
+          <MyStudyCard title="실패" value="23" unit="회" />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-[16px]">
+        <div className="flex items-center gap-[var(--spacing-75)]">
+          <Image src="icons/graph_icon.svg" alt="file" width={32} height={24} />
+          <div className="text-[20px] leading-9 font-[var(--font-weight-bold)]">
+            성장 지표
           </div>
-          <div className="flex flex-1 flex-col gap-[var(--spacing-150)] rounded-[var(--radius-100)] bg-[var(--color-background-alternative)] p-[var(--spacing-250)] text-[var(--color-text-default)]">
-            <div className="text-left text-[15px] font-[var(--font-weight-regular)]">
-              연속 참여
-            </div>
-            <div className="text-right text-[24px] font-[var(--font-weight-bold)]">
-              173
-            </div>
-          </div>
-          <div className="flex flex-1 flex-col gap-[var(--spacing-150)] rounded-[var(--radius-100)] bg-[var(--color-background-alternative)] p-[var(--spacing-250)] text-[var(--color-text-default)]">
-            <div className="text-left text-[15px] font-[var(--font-weight-regular)]">
-              완료
-            </div>
-            <div className="text-right text-[24px] font-[var(--font-weight-bold)]">
-              173
-            </div>
-          </div>
+        </div>
+
+        <div className="flex gap-[var(--spacing-200)]">
+          <MyStudyCard title="총 참여" value="173" />
+          <MyStudyCard title="등급" value="S" unit="등급" />
+          <MyStudyCard title="완료" value="23회" />
         </div>
       </div>
     </div>
