423a41d96367dec801a0c4f6971a95d40b5d828d
James <junyoungcat@gmail.com>
2025-05-17 17:56:17 +0900

    feat: add chip stories

--- src/shared/ui/chip/index.tsx
+++ src/shared/ui/chip/index.tsx
@@ -1,17 +1,35 @@
 import { cn } from '@/shared/shadcn/lib/utils';
+import { cva } from 'class-variance-authority';
 
 interface Props {
   text: string;
   isActive?: boolean;
+  className?: string;
 }
 
-function Chip({ text, isActive = false }: Props) {
+const chipVariants = cva(
+  ' itmes-center flex min-h-[40px] justify-center rounded-full border border-[var(--border)] p-[8px] text-[14px] leading-[22px] font-[500] text-[var(--color-text-default)]',
+  {
+    variants: {
+      color: {
+        default:
+          'bg-[var(--color-background-default)] text-text-subtlest border-[var(--color-border-default)]',
+        isActive:
+          'bg-[var(--color-fill-brand-default-default)] text-[var(--color-text-inverse)] border-none',
+      },
+    },
+    defaultVariants: {
+      color: 'default',
+    },
+  },
+);
+
+function Chip({ text, isActive = false, className }: Props) {
   return (
     <div
       className={cn(
-        'itmes-center flex min-h-[40px] justify-center rounded-full border border-[var(--border)] p-[8px] text-[14px] leading-[22px] font-[500] text-[var(--color-text-default)]',
-        isActive && 'bg-[var(--color-fill-success-default)]',
-        isActive && 'text-[var(--color-text-inverse)]',
+        chipVariants({ color: isActive ? 'isActive' : 'default' }),
+        className,
       )}
     >
       {text}
