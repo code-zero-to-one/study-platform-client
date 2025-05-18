023538e0c2f7cf1dff36df866c48c2f3ed2b982e
조민주 <pida6716@gmail.com>
2025-05-18 23:36:08 +0900

    refactor: home-ui 폴더로 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Banner from '@/widgets/home/banner';
-import DateSelector from '@/features/home/data-selector';
+import DateSelector from '@/features/home/ui/data-selector';
 import Sidebar from '@/widgets/home/sidebar';
 import StudyListSection from '@/widgets/home/study-list-table';
-import TodayStudyCard from '@/features/home/today-study-card';
+import TodayStudyCard from '@/features/home/ui/today-study-card';
 
 const mockData = [
   {
