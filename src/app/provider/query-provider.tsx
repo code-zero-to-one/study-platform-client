e846a761890f973b72a584ff8924fbb3ddcd1e0f
조민주 <pida6716@gmail.com>
2025-05-22 13:18:20 +0900

    refactor: kebab case 변경

--- src/app/provider/query-provider.tsx
+++ src/app/provider/query-provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { getQueryClient } from '@/shared/tanstack-query/queryClient';
+import { getQueryClient } from '@/shared/tanstack-query/query-client';
 
 export default function QueryProvider({
   children,
