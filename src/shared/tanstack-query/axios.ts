05db3ad514203490ca0d34f1f628cd28ef4d81ed
조민주 <pida6716@gmail.com>
2025-05-24 22:36:37 +0900

    feat: study 관련 api 추가

--- src/shared/tanstack-query/axios.ts
+++ src/shared/tanstack-query/axios.ts
@@ -10,10 +10,14 @@ export const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token =
-      typeof window !== 'undefined'
-        ? localStorage.getItem('accessToken')
-        : null;
+    // const token =
+    //   typeof window !== 'undefined'
+    //     ? localStorage.getItem('accessToken')
+    //     : null;
+
+    // 임시로 토큰 지정 후 사용
+    const token = '4/0Ab_5qlmuLtSUyrrO0';
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -25,10 +29,23 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => {
-    // 예: 401이면 로그아웃 처리 등
-    if (error.response?.status === 401) {
-      // handleUnauthorized()
+  async (error) => {
+    // error.config에는 실패한 요청의 모든 설정(URL, 헤더, 데이터 등)이 포함
+    const originalRequest = error.config;
+
+    // 401 에러(인증 실패) 발생시 토큰 갱신 후 실패한 요청을 재시도
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const res = await axiosInstance.get('/api/v1/auth/access-token/refresh');
+      const newAccessToken = res.data.accessToken;
+
+      if (newAccessToken) {
+        localStorage.setItem('accessToken', newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return axiosInstance(originalRequest);
+      }
     }
 
     return Promise.reject(error);
