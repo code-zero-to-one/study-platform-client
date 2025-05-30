24e536c3ebd1da082131699eb73ab19eaafab288
조민주 <pida6716@gmail.com>
2025-05-30 23:18:46 +0900

    chore: 임시 axios 추가 (임시)
    이후 성진님과 pr 후 변경 예정

--- src/shared/tanstack-query/axios.ts
+++ src/shared/tanstack-query/axios.ts
@@ -27,27 +27,27 @@ axiosInstance.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    // error.config에는 실패한 요청의 모든 설정(URL, 헤더, 데이터 등)이 포함
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     // error.config에는 실패한 요청의 모든 설정(URL, 헤더, 데이터 등)이 포함
+//     const originalRequest = error.config;
 
-    // 401 에러(인증 실패) 발생시 토큰 갱신 후 실패한 요청을 재시도
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+//     // 401 에러(인증 실패) 발생시 토큰 갱신 후 실패한 요청을 재시도
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
 
-      const res = await axiosInstance.get('/api/v1/auth/access-token/refresh');
-      const newAccessToken = res.data.accessToken;
+//       const res = await axiosInstance.get('/api/v1/auth/access-token/refresh');
+//       const newAccessToken = res.data.accessToken;
 
-      if (newAccessToken) {
-        localStorage.setItem('accessToken', newAccessToken);
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+//       if (newAccessToken) {
+//         localStorage.setItem('accessToken', newAccessToken);
+//         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 
-        return axiosInstance(originalRequest);
-      }
-    }
+//         return axiosInstance(originalRequest);
+//       }
+//     }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
