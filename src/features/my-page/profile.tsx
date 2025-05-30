fc3955f840db1aaed60a58c68eb8adaab535e50e
조민주 <pida6716@gmail.com>
2025-05-30 22:32:52 +0900

    refactor: var 삭제 및 Badge 변경

--- src/features/my-page/profile.tsx
+++ src/features/my-page/profile.tsx
@@ -1,94 +1,77 @@
 'use client';
 
 import Image from 'next/image';
-import { Badge } from '@/shared/shadcn/ui/badge';
+import UserAvatar from '@/shared/ui/avatar';
+import { Badge } from '@/shared/ui/badge';
 import ProfileEditModal from './Profile-edit-modal';
 
 export default function Profile() {
   return (
-    <div className="flex gap-[24px] px-[16px]">
-      <div className="h-[100px] w-[180px] rounded-[100px] bg-[var(--color-background-alternative)]" />
-
-      <div className="flex flex-col gap-[32px] pt-[12px]">
-        <div className="flex flex-col gap-[24px]">
+    <div className="flex gap-300 px-200">
+      <UserAvatar size={90} />
+      <div className="flex flex-col gap-400">
+        <div className="flex flex-col gap-300">
           {/* 이름, 소개, 배찌들 */}
-          <div className="flex flex-col gap-[6px]">
+          <div className="flex flex-col gap-75">
             {/* 배찌들 */}
-            <div className="flex gap-[4px]">
-              <Badge
-                variant="secondary"
-                className="rounded-[4px] bg-[var(--color-green-500)] pt-[4px] pr-[8px] pb-[4px] pl-[8px]"
-              >
-                Badge1
-              </Badge>
-              <Badge
-                variant="secondary"
-                className="rounded-[4px] bg-[var(--color-blue-500)] pt-[4px] pr-[8px] pb-[4px] pl-[8px]"
-              >
-                Badge2
-              </Badge>
+            <div className="flex gap-50">
+              <Badge color='orange'>ENFJ</Badge>
+              <Badge color='orange'>IT</Badge>
+              <Badge color='gray'>AI</Badge>
+              <Badge color='gray'>Back-end</Badge>
+              <Badge color='purple'>플라잉요가</Badge>
+              <Badge color='purple'>사진</Badge>
             </div>
-            {/* 이름 */}
-            <div className="text-blue text-[24px] leading-9 font-bold">
+            <div className="font-designer-28b">
               신채호
             </div>
 
-            <p className="text-[15px] leading-[23px] font-[500] text-[var(--color-text-default)]">
-              함께 성장하는 개발자가 되고 싶어요.함께 성장하는 개발자가 되고
-              싶어요.함께 성장하는 개발자가 되고 싶어요.함께 성장하는 개발자가
-              되고 싶어요.최대 글자수 두줄까지 두줄이상 말줄임 처리
+            <p className="font-designer-15m text-text-default">
+              함께 성장하는 개발자가 되고 싶어요.<br />이 스터디를 통해서 함께 성장해나가며 백엔드와 AI의 마스터가 되고 싶습니다. 잘 부탁드립니다.
             </p>
           </div>
 
           {/* 생년월일, 전화번호, 깃허브, 웹사이트 */}
           <div className="flex gap-[20px]">
             <div className="flex flex-col gap-[8px]">
-              <div className="flex gap-[4px]">
+              <div className="flex gap-100 font-designer-14r text-text-subtle">
                 <Image
                   src="icons/Cake.svg"
                   alt="Profile"
                   width={16}
                   height={16}
                 />
-                <div className="text-[12px] text-[var(--color-text-subtlest)]">
-                  1999.01.01
-                </div>
+                1999.01.01
               </div>
-              <div className="flex gap-[4px]">
+              <div className="flex gap-100 font-designer-14r text-text-subtle">
                 <Image
                   src="icons/Phone.svg"
                   alt="Profile"
                   width={16}
                   height={16}
                 />
-                <div className="text-[12px] text-[var(--color-text-subtlest)]">
-                  010-1234-5678
-                </div>
+                010-1234-5678
               </div>
             </div>
 
             <div className="flex flex-col gap-[8px]">
-              <div className="flex gap-[4px]">
+              <div className="flex gap-100 font-designer-14r text-text-subtle">
                 <Image
                   src="icons/GithubLogo.svg"
                   alt="Profile"
                   width={16}
                   height={16}
                 />
-                <div className="text-[12px] text-[var(--color-text-subtlest)]">
-                  https://github.com/@zero-one
-                </div>
+                https://github.com/@zero-one
               </div>
-              <div className="flex gap-[4px]">
+              <div className="flex gap-100 font-designer-14r text-text-subtle">
                 <Image
                   src="icons/GlobeSimple.svg"
                   alt="Profile"
                   width={16}
                   height={16}
                 />
-                <div className="text-[12px] text-[var(--color-text-subtlest)]">
-                  https://zero-one.com
-                </div>
+                https://zero-one.com
               </div>
             </div>
           </div>
