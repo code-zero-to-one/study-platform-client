9fe763b4dce8931ac6377db1674316b7622071d8
조민주 <pida6716@gmail.com>
2025-06-10 01:16:22 +0900

    chore: csr ssr 분리 및 주차 값 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import DateSelector from '@/features/study/ui/data-selector';
-import StudyListSection from '@/features/study/ui/study-list-table';
-import TodayStudyCard from '@/features/study/ui/today-study-card';
+import StudyCard from '@/features/study/ui/study-card';
 import Banner from '@/widgets/home/banner';
 import Sidebar from '@/widgets/home/sidebar';
 
@@ -9,14 +7,7 @@ export default async function Home() {
     <div className='container mx-auto min-h-screen py-600 flex gap-600'>
       <div className='flex-1 flex flex-col gap-500'>
         <Banner />
-        <div className='flex flex-col gap-300'>
-          <div className='font-bold-h3'>3월 1주차 스터디</div>
-          <DateSelector />
-        </div>
-        <div className='p-400 border border-border-default rounded-200 flex flex-col gap-500'>
-          <TodayStudyCard />
-          <StudyListSection />
-        </div>
+        <StudyCard />
       </div>
 
       <aside className='w-[335px] shrink-0'>
---
+++ src/features/study/ui/study-card.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { startOfMonth, differenceInCalendarWeeks, getMonth } from 'date-fns';
+import { useState } from 'react';
+import DateSelector from './data-selector';
+import StudyListSection from './study-list-table';
+import TodayStudyCard from './today-study-card';
+
+function getWeekOfMonth(date: Date) {
+   const start = startOfMonth(date);
+
+   return differenceInCalendarWeeks(date, start, { weekStartsOn: 1 });
+}
+
+export default function StudyCard() {
+   const [selectedDate, setSelectedDate] = useState(new Date());
+
+   const month = getMonth(selectedDate) + 1; // 0-based → 1-based
+   const week = getWeekOfMonth(selectedDate);
+
+   return (
+      <>
+         <div className="flex flex-col gap-300">
+            <div className="font-bold-h3">{`${month}월 ${week}주차 스터디`}</div>
+            <DateSelector value={selectedDate} onChange={setSelectedDate} />
+         </div>
+         <div className='p-400 border border-border-default rounded-200 flex flex-col gap-500'>
+            <TodayStudyCard date={selectedDate} />
+            <StudyListSection date={selectedDate} />
+         </div >
+      </>
+   );
+}
