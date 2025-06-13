b64a25eb26c25743e34f5416198fea8d7356d5a9
조민주 <pida6716@gmail.com>
2025-06-14 01:15:07 +0900

    feat: ssr 방식 변경

--- src/entities/user/model/use-user-profile-query.ts
+++ src/entities/user/model/use-user-profile-query.ts
@@ -6,6 +6,7 @@ export const useUserProfileQuery = (memberId: number) => {
   return useQuery<GetUserProfileResponse>({
     queryKey: ['userProfile', memberId],
     queryFn: () => getUserProfile(memberId),
+    enabled: !!memberId,
     staleTime: 1000 * 60 * 5, // 5분 캐싱 (네 프로젝트 성격 따라 조절)
   });
 };
