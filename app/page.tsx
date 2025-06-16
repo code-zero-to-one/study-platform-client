c30d55ebaca9617841d81f09fa51be0e8041fb6f
조민주 <pida6716@gmail.com>
2025-06-16 19:29:03 +0900

    feat: 비로그인시 로그인 페이지 리다이렉션 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
+import { redirect } from 'next/navigation';
 import StudyCard from '@/features/study/ui/study-card';
+import { getServerCookie } from '@/shared/lib/server-cookie';
 import Banner from '@/widgets/home/banner';
 import Sidebar from '@/widgets/home/sidebar';
 
 export default async function Home() {
+  const memberIdStr = await getServerCookie('memberId');
+
+  if (!memberIdStr) {
+    redirect('/login');
+  }
+  const memberId = Number(memberIdStr);
+
+  if (isNaN(memberId) || memberId <= 0) {
+    redirect('/login');
+  }
+
   return (
-    <div className='container mx-auto min-h-screen py-600 flex gap-600'>
-      <div className='flex-1 flex flex-col gap-500'>
+    <div className="container mx-auto flex min-h-screen gap-600 py-600">
+      <div className="flex flex-1 flex-col gap-500">
         <Banner />
         <StudyCard />
       </div>
 
-      <aside className='w-[335px] shrink-0'>
+      <aside className="w-[335px] shrink-0">
         <Sidebar />
       </aside>
     </div>
