00a7fc85abeae66f1075f7d525050dfac50e1bb6
조민주 <pida6716@gmail.com>
2025-06-16 19:02:16 +0900

    fix: ssr용 쿠키 코드 변경

--- src/shared/lib/server-cookie.ts
+++ src/shared/lib/server-cookie.ts
@@ -1,22 +1,10 @@
-import { headers } from 'next/headers';
+import { cookies } from 'next/headers';
 
 export const getServerCookie = async (
   name: string,
 ): Promise<string | undefined> => {
-  const allHeaders = await headers();
-  const cookieHeader = allHeaders.get('cookie') as string | null;
+  const cookieStore = await cookies();
+  const value = cookieStore.get(name)?.value;
 
-  if (!cookieHeader) return undefined;
-
-  const cookiesArray = cookieHeader.split('; ').map((c) => {
-    const [key, ...rest] = c.split('=');
-    const value = rest.join('=');
-
-    return [key, value] as [string, string];
-  });
-
-  const cookiesMap = new Map<string, string>(cookiesArray);
-  const value = cookiesMap.get(name);
-
-  return value ? decodeURIComponent(value) : undefined;
+  return value ?? undefined;
 };
