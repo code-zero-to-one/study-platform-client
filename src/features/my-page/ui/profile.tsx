ac0f63c4320205971331ff7883a2ab02d84caec2
조민주 <pida6716@gmail.com>
2025-06-18 19:29:51 +0900

    fix: 파일 네이밍 수정

--- src/features/my-page/ui/profile.tsx
+++ src/features/my-page/ui/profile.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { MemberProfile } from '@/entities/user/api/types';
 import UserAvatar from '@/shared/ui/avatar';
 import Badge from '@/shared/ui/badge';
-import ProfileEditModal from './Profile-edit-modal';
+import ProfileEditModal from './profile-edit-modal';
 import { UpdateUserProfileRequest } from '../api/types';
 import { useUpdateUserProfileMutation } from '../model/use-update-user-profile-mutation';
 
