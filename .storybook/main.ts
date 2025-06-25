dd64608cafa20aa71d7db41d7fca2b6e8ed87025
yeonhwan <yeonhwan619@gmail.com>
2025-06-25 14:13:19 +0900

    feat: widget/banner.stories 추가 vite-plugin-svgr 추가
    Widget 에 포함되어있는 SVGR 을 지원하기위해 vite-plugin-svgr 및 관련 설정코드 추

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
+import svgr from 'vite-plugin-svgr';
 
 const config: StorybookConfig = {
   stories: [
@@ -15,5 +16,37 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../public'],
+  viteFinal: (config) => {
+    // SVGR plugin이 Storybook 내 Vite 번들링에 사용되도록 추가
+    config.plugins = [...(config.plugins || []), svgr({ include: /\.svg$/ })];
+
+    // nextjs-vite 내에서 SVGR 이 작동하도록 plugin 동작 수정
+    // 출처: https://github.com/ygkn/storybook-nextjs-vite-svgr/blob/main/.storybook/main.ts
+    config.plugins = config.plugins!.flat().map((plugin) => {
+      if (
+        typeof plugin === 'object' &&
+        plugin !== null &&
+        'name' in plugin &&
+        plugin.name === 'vite-plugin-storybook-nextjs-image'
+      ) {
+        return {
+          ...plugin,
+          resolveId(id, importer) {
+            if (id.endsWith('.svg')) {
+              return null;
+            }
+
+            // @ts-expect-error `resolveId` hook of vite-plugin-storybook-nextjs-image is a function
+            return plugin.resolveId(id, importer);
+          },
+        };
+      }
+      return plugin;
+    });
+
+    config.plugins = [...config.plugins];
+
+    return config;
+  },
 };
 export default config;
