6abc06d37c759ae72ec123dd49b5fba9dd5df174
yeonhwan <yeonhwan619@gmail.com>
2025-06-25 14:04:20 +0900

    fix: Input.stories 수정

--- src/stories/ui/input.stories.tsx
+++ src/stories/ui/input.stories.tsx
@@ -1,20 +1,57 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { ChangeEvent } from 'react';
+import { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { fn } from 'storybook/test';
 import { BaseInput } from '@/shared/ui/input';
 
-const meta: Meta<typeof BaseInput> = {
+const meta = {
+  title: 'UI/Input',
   component: BaseInput,
-};
+  tags: ['autodocs'],
+  argTypes: {
+    placeholder: {
+      description: 'Input 플레이스홀더 레이블',
+      control: 'text',
+    },
+    onChange: {
+      description: 'Input 값 변경 핸들러',
+      control: false,
+    },
+    color: {
+      description: 'Input validation 상태에 따른 색 변경',
+      control: { type: 'select' },
+    },
+  },
+} satisfies Meta<typeof BaseInput>;
 
 export default meta;
-
 type Story = StoryObj<typeof BaseInput>;
+
+// --- Stories ---
 
 export const Default: Story = {
   args: {
     placeholder: 'Search',
-    onChange: (event: ChangeEvent<HTMLInputElement>) => {
-      console.log('onChange', event.target.value);
+    onChange: () => {
+      fn();
+    },
+  },
+};
+
+export const Error: Story = {
+  args: {
+    placeholder: 'Error',
+    color: 'error',
+    onChange: () => {
+      fn();
+    },
+  },
+};
+
+export const Success: Story = {
+  args: {
+    placeholder: 'Success',
+    color: 'success',
+    onChange: () => {
+      fn();
     },
   },
 };
