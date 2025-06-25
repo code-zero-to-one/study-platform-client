304c2c50553369a99d36df4a736f5ba95b6fb35c
yeonhwan <yeonhwan619@gmail.com>
2025-06-25 14:06:58 +0900

    fix: Toggle.stories 수정

--- src/stories/ui/toggle.stories.tsx
+++ src/stories/ui/toggle.stories.tsx
@@ -1,48 +1,80 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { useState, ComponentProps } from 'react';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 import { ToggleSwitch } from '@/shared/ui/toggle/switch';
 
-type ToggleSwitchProviderProps = ComponentProps<typeof ToggleSwitch.Root>;
-
-const meta: Meta<typeof ToggleSwitch.Root> = {
+const meta = {
+  title: 'UI/Toggle Switch',
   component: ToggleSwitch.Root,
+  parameters: {
+    layout: 'centered',
+  },
   tags: ['autodocs'],
   argTypes: {
+    checked: {
+      description: 'Toggle ON/OFF 유무',
+      control: 'boolean',
+    },
     color: {
+      description: 'Toggle 백그라운드 색 변경',
       control: 'select',
       options: ['primary', 'gray'],
     },
     size: {
+      description: 'Toggle 크기 변경',
       control: 'select',
       options: ['sm', 'md', 'lg'],
     },
     disabled: {
+      description: 'Toggle 비활성화 유무',
       control: 'boolean',
     },
   },
   args: {
     color: 'primary',
     size: 'md',
+    checked: false,
     disabled: false,
   },
-};
+} satisfies Meta<typeof ToggleSwitch.Root>;
 export default meta;
 
 type Story = StoryObj<typeof ToggleSwitch.Root>;
 
-const ToggleWithState = (args: ToggleSwitchProviderProps) => {
-  const [checked, setChecked] = useState(false);
-
-  return (
-    <ToggleSwitch.Root
-      {...args}
-      checked={checked}
-      onCheckedChange={setChecked}
-    />
-  );
-};
+// --- Stories ---
 
 export const Default: Story = {
-  render: ToggleWithState,
   args: {},
+};
+
+export const ToggleOn: Story = {
+  args: {
+    checked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Gray: Story = {
+  args: {
+    color: 'gray',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+export const Medium: Story = {
+  args: {
+    size: 'md',
+  },
+};
+export const Large: Story = {
+  args: {
+    size: 'lg',
+  },
 };
