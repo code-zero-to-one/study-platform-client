41222b1e46252f43882fa9c704bfe121cbced4ef
SEONG JIN JO <123255935+seong-jin-jo@users.noreply.github.com>
2025-06-27 15:35:37 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,6 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  console.log("NEXT_PUBLIC_API_BASE_URL", process.env.NEXT_PUBLIC_API_BASE_URL);
-  
   const memberId = await getLoginUserId();
 
   if (!memberId) {
