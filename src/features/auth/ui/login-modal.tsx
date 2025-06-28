bd5ae5b3d3e399228302561746c4f116721afe2d
조성진 <sj@joseongjin-ui-MacBookPro.local>
2025-06-28 14:45:28 +0900

    fix : 구글 클라이언트도 시크릿키로 관리

--- src/features/auth/ui/login-modal.tsx
+++ src/features/auth/ui/login-modal.tsx
@@ -25,11 +25,12 @@ export default function LoginModal({
 
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
   const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID; // 테스트 운영서버 분기 필요
+  const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID; // 테스트 운영서버 분기 필요
 
   // TODO : 실제 백엔드에서 제공하는 URL로 교체필요
   const NAVER_LOGIN_URL = '';
   const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${API_BASE_URL}/api/v1/auth/kakao/redirect-uri&response_type=code&state=${state}`;
-  const GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20profile&access_type=offline&prompt=consent&include_granted_scopes=true&response_type=code&redirect_uri=${API_BASE_URL}/api/v1/auth/google/redirect-uri&client_id=616205933420-b45d510q23togkaqo069j8igmsjhp9v0.apps.googleusercontent.com&state=${state}`;
+  const GOOGLE_LOGIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20profile&access_type=offline&prompt=consent&include_granted_scopes=true&response_type=code&redirect_uri=${API_BASE_URL}/api/v1/auth/google/redirect-uri&client_id=${GOOGLE_CLIENT_ID}&state=${state}`;
 
   return (
     <Modal.Root open={open} onOpenChange={onClose}>
