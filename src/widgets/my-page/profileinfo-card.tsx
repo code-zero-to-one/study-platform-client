4d130d9ed7bb38c6555d6f7c61f673e0f4141771
조민주 <pida6716@gmail.com>
2025-07-03 19:47:02 +0900

    feat: 유저 프로필 모달 추가

--- src/widgets/my-page/profileinfo-card.tsx
+++ src/widgets/my-page/profileinfo-card.tsx
@@ -5,8 +5,8 @@ interface Props {
 
 export default function ProfileInfoCard({ title, content }: Props) {
   return (
-    <div className="rounded-75 bg-background-alternative flex gap-500 p-250">
-      <div className="font-designer-16b text-text-default w-1/6 shrink-0">
+    <div className="rounded-75 bg-background-alternative flex gap-400 p-250">
+      <div className="font-designer-16b text-text-default w-1/5 shrink-0">
         {title}
       </div>
       <div className="font-designer-15r text-text-default flex-1">
