dc077e32bdd50d0aeb7c25bc94d6db70a8937214
조민주 <pida6716@gmail.com>
2025-07-03 23:56:40 +0900

    style: UI 수정

--- src/widgets/my-study/my-study-card.tsx
+++ src/widgets/my-study/my-study-card.tsx
@@ -2,22 +2,38 @@ interface Props {
   title: string;
   value: React.ReactNode;
   unit?: string;
+  changeIndicator?: {
+    type: 'increase' | 'decrease';
+    value: number;
+  };
 }
 
-export default function MyStudyCard({ title, value, unit }: Props) {
+export default function MyStudyCard({
+  title,
+  value,
+  unit,
+  changeIndicator,
+}: Props) {
   return (
-    <div className="flex flex-1 flex-col gap-[var(--spacing-150)] rounded-[var(--radius-100)] bg-[var(--color-background-alternative)] p-[var(--spacing-250)] text-[var(--color-text-default)]">
-      <div className="text-left text-[15px] font-[var(--font-weight-regular)]">
-        {title}
-      </div>
-      <div className="flex items-center justify-end gap-[var(--spacing-50)] text-right">
-        <span className="text-[24px] font-[var(--font-weight-bold)]">
-          {value}
-        </span>
-        {unit && (
-          <span className="text-[16px] font-[var(--font-weight-medium)]">
-            {unit}
-          </span>
+    <div className="bg-background-alternative rounded-75 text-text-default flex flex-1 flex-col gap-150 p-250">
+      <div className="font-designer-15r">{title}</div>
+      <div className="flex items-center justify-end gap-75 text-right">
+        <span className="font-designer-24b">{value}</span>
+        {unit && <span className="font-designer-16m">{unit}</span>}
+        {changeIndicator && (
+          <div
+            className={`font-designer-12m rounded-25 px-75 py-[3px] ${
+              changeIndicator.type === 'increase'
+                ? 'text-icon-information bg-blue-100'
+                : 'text-icon-error bg-red-100'
+            } flex items-center gap-25`}
+          >
+            {changeIndicator.type === 'increase' ? '▲' : '▼'}{' '}
+            <span className="font-designer-15b px-[2px]">
+              {changeIndicator.value}
+            </span>
+            %
+          </div>
         )}
       </div>
     </div>
