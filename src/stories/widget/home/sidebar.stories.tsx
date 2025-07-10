e13a78c2589bb486777eee6f19581f97e300c017
yeonhwan <yeonhwan619@gmail.com>
2025-07-10 19:38:46 +0900

    feat: widget/home/sidebar.stories 추가 및 원 컴포넌트 리팩토링

---
+++ src/stories/widget/home/sidebar.stories.tsx
@@ -0,0 +1,138 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { ISidebarProps, SidebarClient as Sidebar } from '@/widgets/home/sidebar';
+
+const MOCK_DATA:ISidebarProps =  {
+    hasTodo: true,
+    memberId: 4,
+    userProfile:{
+    autoMatching: true,
+    memberId: 4,
+    memberInfo: {
+      availableStudyTimes: [
+        {
+          fromTime: "09:00",
+          fullLabel: "오전(09:00~12:00)",
+          id: 1,
+          label: "오전",
+          toTime: "12:00"
+        },
+        {
+          fromTime: null,
+          fullLabel: "시간 협의 가능",
+          id: 6,
+          label: "시간 협의 가능",
+          toTime: null
+        }
+      ],
+      preferredStudySubject: {
+        name: "전체",
+        studySubjectId: "ALL"
+      },
+      selfIntroduction: "안녕하세요",
+      studyPlan: "반갑습니다",
+      techStacks: [
+        {
+          code: "DB",
+          level: 2,
+          parentId: 1,
+          techStackId: 5,
+          techStackName: "Database"
+        },
+        {
+          code: "DJA",
+          level: 2,
+          parentId: 1,
+          techStackId: 2,
+          techStackName: "Django"
+        }
+      ]
+    },
+    memberProfile: {
+      birthDate: null,
+      blogOrSnsLink: null,
+      githubLink: null,
+      hobbies: null,
+      interests: [
+        {
+          id: 6,
+          name: "음악"
+        },
+        {
+          id: 7,
+          name: "종이접기"
+        },
+        {
+          id: 8,
+          name: "영화/드라마"
+        },
+        {
+          id: 9,
+          name: "운동/헬스"
+        }
+      ],
+      mbti: null,
+      memberName: "조민주",
+      profileImage: {
+        imageId: 16,
+        resizedImages: [
+          {
+            imageSizeType: {
+              height: null,
+              imageTypeName: "ORIGINAL",
+              width: null
+            },
+            resizedImageId: 16,
+            resizedImageUrl: "https://test-api.zeroone.it.kr/profile-image/profile-formdata-4"
+          }
+        ]
+      },
+      simpleIntroduction: "기본 이미지로 변경했어요",
+      tel: "010-1122-2233"
+    }
+}}
+
+
+const meta = {
+  title: 'Widget/Home/Sidebar',
+  beforeEach: async () => {},
+  parameters: {
+    layout: 'centered'
+  },
+  component: Sidebar,
+} satisfies Meta<typeof Sidebar>;
+
+export default meta;
+
+type Story = StoryObj<typeof Sidebar>;
+
+export const Logout: Story = {
+  parameters: {
+    id: 'logout',
+  },
+  argTypes: {
+    memberId: {
+      control: false
+    },
+    userProfile: {
+      control: false
+    },
+    hasTodo : {
+    control: false
+  }
+  }
+};
+
+export const Login: Story = {
+  parameters: {
+    id: 'login',
+  },
+  args: MOCK_DATA,
+  argTypes: {
+    memberId: {
+      control: false,
+    },
+    hasTodo : {
+    control: 'boolean'
+  }
+  }
+};
--- src/widgets/home/sidebar.tsx
+++ src/widgets/home/sidebar.tsx
@@ -1,16 +1,22 @@
 import { getUserProfile } from '@/entities/user/api/get-user-profile';
+import { GetUserProfileResponse } from '@/entities/user/api/types';
 import MyProfileCard from '@/features/study/ui/my-profile-card';
 import StartStudyModal from '@/features/study/ui/start-study-modal';
 import { getLoginUserId } from '@/shared/lib/get-login-user';
 import Calendar from '@/widgets/home/calendar';
 import TodoList from '@/widgets/home/todo-list';
 
-export default async function Sidebar() {
-  const memberId = await getLoginUserId();
+export interface ISidebarProps {
+  memberId: number;
+  userProfile: GetUserProfileResponse;
+  hasTodo: boolean;
+}
 
-  const userProfile = await getUserProfile(memberId);
-
-  const hasTodo = false; // 나중에 스터디 참여 유무로 변경할 예정
+export const SidebarClient = ({
+  userProfile,
+  memberId,
+  hasTodo,
+}: ISidebarProps) => {
 
   return (
     <aside className="flex flex-col gap-300">
@@ -37,5 +43,21 @@ export default async function Sidebar() {
       )}
       <Calendar />
     </aside>
+  );
+};
+
+export default async function Sidebar() {
+  const memberId = await getLoginUserId();
+
+  const userProfile = await getUserProfile(memberId);
+
+  const hasTodo = false; // 나중에 스터디 참여 유무로 변경할 예정
+
+  return (
+    <SidebarClient
+      memberId={memberId}
+      userProfile={userProfile}
+      hasTodo={hasTodo}
+    />
   );
 }
