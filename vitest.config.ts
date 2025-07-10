40e6071940cfaf442cb8d191fdfcfe0696e3734a
yeonhwan <yeonhwan619@gmail.com>
2025-07-10 19:35:29 +0900

    fix: MSW, Vitest 관련 Storybook 환경설정 정리

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+// @ts-expect-error: Library issues
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import { defineConfig } from 'vitest/config';
 
 const dirname =
@@ -9,7 +9,7 @@ const dirname =
     ? __dirname
     : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/writing-tests/test-addon
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   test: {
     workspace: [
@@ -17,7 +17,7 @@ export default defineConfig({
         extends: true,
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({ configDir: path.join(dirname, '.storybook') }),
         ],
         test: {
@@ -25,8 +25,8 @@ export default defineConfig({
           browser: {
             enabled: true,
             headless: true,
-            name: 'chromium',
             provider: 'playwright',
+            instances: [{ browser: 'chromium' }],
           },
           setupFiles: ['.storybook/vitest.setup.ts'],
         },
