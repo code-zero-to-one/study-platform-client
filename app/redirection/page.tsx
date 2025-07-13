59c4fcc48f7547fbaf22c1e0198006f03ec925d6
조성진 <sj@joseongjin-ui-MacBookPro.local>
2025-07-13 23:44:39 +0900

    chore : 주석제거, 로그인 아이콘 사이 gap 설정

--- app/redirection/page.tsx
+++ app/redirection/page.tsx
@@ -31,14 +31,6 @@ function RedirectionContent() {
 
         if (isGuest === 'true') {
           router.push('/sign-up');
-          // isGuest가 true 인 경우 memberId가 없어서 아래 hashValue 에서 에러가 남.
-          // 따라서 해당 이벤트는 회원가입 직후인 'sign-up-modal.tsx' 로 이동.
-
-          // sendGTMEvent({
-          //   event: 'custom_member_join',
-          //   dl_timestamp: new Date().toISOString(),
-          //   dl_member_id: hashValue(memberId),
-          // });
         } else {
           router.push('/');
           router.refresh();
