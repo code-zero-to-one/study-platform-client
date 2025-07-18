34985fa4820f4f66455ee26f34f6cdd361e5dfbb
yeonhwan <yeonhwan619@gmail.com>
2025-07-18 21:19:04 +0900

    chore: calendar.stories 필요 없는 주석 대체

--- src/stories/ui/calendar.stories.tsx
+++ src/stories/ui/calendar.stories.tsx
@@ -1,37 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite';
-// import { http, HttpResponse } from 'msw';
 import Calendar from '@/widgets/home/calendar';
 
-// const calendarMockData = {};
-
-//TODO: API request Mocking
-
+// TODO: 현재는 문제 없는 것 같지만 추후에 API mocking 적용 할지 확인
 const meta: Meta<typeof Calendar> = {
   title: 'UI/Calendar',
   component: Calendar,
   tags: ['autodocs'],
-  // parameters: {
-  //   layout: 'centered',
-  //   msw: {
-  //     handlers: [
-  //       http.get(
-  //         `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/study/daily/month`,
-  //         () => {
-  //           return HttpResponse.json(calendarMockData);
-  //         },
-  //       ),
-  //     ],
-  //   },
-  // },
-  // decorators: [
-  //   (Story) => {
-  //     return (
-  //       <div style={{ width: '335px' }}>
-  //         <Story />
-  //       </div>
-  //     );
-  //   },
-  // ],
 };
 
 export default meta;
