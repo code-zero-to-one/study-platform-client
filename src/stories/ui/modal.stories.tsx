c405d842f6455b862863c6a2606d4012dd0a4f95
yeonhwan <yeonhwan619@gmail.com>
2025-07-18 21:19:40 +0900

    refactor: modal.stories meta 정보 추가 및 리팩토링

--- src/stories/ui/modal.stories.tsx
+++ src/stories/ui/modal.stories.tsx
@@ -1,44 +1,50 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
 import { XIcon } from 'lucide-react';
 import { Modal } from '@/shared/ui/modal';
 
-const meta: Meta<typeof Modal> = {};
+const ExampleModal = () => {
+  return (
+    <Modal.Root>
+      <Modal.Trigger>Open Modal</Modal.Trigger>
+      <Modal.Portal>
+        <Modal.Overlay />
+        <Modal.Content>
+          <Modal.Header className="flex items-center justify-between">
+            <Modal.Title>Modal Header</Modal.Title>
+            <Modal.Close>
+              <XIcon />
+            </Modal.Close>
+          </Modal.Header>
+          <Modal.Body>Modal Body</Modal.Body>
+          <Modal.Footer className="flex justify-end">
+            <Modal.Close asChild>
+              <button
+                className="cursor-pointer"
+                onClick={() => {
+                  console.log('확인 요청하기');
+                }}
+              >
+                확인
+              </button>
+            </Modal.Close>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal.Portal>
+    </Modal.Root>
+  );
+};
+
+const meta: Meta<typeof Modal> = {
+  title: 'UI/Modal',
+  parameters: {
+    layout: 'centered',
+  },
+  component: ExampleModal,
+  tags: ['autodocs'],
+};
 
 export default meta;
 
 type Story = StoryObj<typeof Modal>;
 
-export const Default: Story = {
-  render: () => {
-    return (
-      <Modal.Root>
-        <Modal.Trigger>Open Modal</Modal.Trigger>
-        <Modal.Portal>
-          <Modal.Overlay />
-          <Modal.Content>
-            <Modal.Header className="flex items-center justify-between">
-              <Modal.Title>Modal Header</Modal.Title>
-              <Modal.Close>
-                <XIcon />
-              </Modal.Close>
-            </Modal.Header>
-            <Modal.Body>Modal Body</Modal.Body>
-            <Modal.Footer className="flex justify-end">
-              <Modal.Close asChild>
-                <button
-                  className="cursor-pointer"
-                  onClick={() => {
-                    console.log('확인 요청하기');
-                  }}
-                >
-                  확인
-                </button>
-              </Modal.Close>
-            </Modal.Footer>
-          </Modal.Content>
-        </Modal.Portal>
-      </Modal.Root>
-    );
-  },
-};
+export const Default: Story = {};
