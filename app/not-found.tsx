ffd7dff710ac82b08314fdfeffade7946d5a4c92
조민주 <pida6716@gmail.com>
2025-07-24 02:58:59 +0900

    fix: warning 제거 (img -> Next/Image)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import Button from '@/shared/ui/button';
 
 export default function NotFound() {
   return (
     <div className="flex h-[calc(100vh-45px)] w-full flex-col items-center justify-center gap-5">
-      <img src="/images/404.png" width={256} height={221} />
+      <Image src="/images/404.png" alt="404 에러" width={256} height={221} />
       <Link href="/">
         <Button size="large" type="button" color="secondary">
           홈으로 이동
