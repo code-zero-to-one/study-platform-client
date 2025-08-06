a6ad874e3dd81638fe7c7291a3009aa89a003033
aken-you <kinsk2839@naver.com>
2025-08-07 00:01:54 +0900

    feat: 인증이 필요없는 axios 인스턴스 생성
    - server, client 둘 다 사용 가능

---
+++ src/shared/tanstack-query/axios.all.ts
@@ -0,0 +1,27 @@
+import axios, { isAxiosError } from 'axios';
+import { ApiError, isApiError } from './api-error';
+
+// * 인증하지 않는 axios 인스턴스
+export const axiosAllInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/`,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+axiosAllInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (
+      error.response &&
+      isAxiosError(error) &&
+      isApiError(error.response.data)
+    ) {
+      throw new ApiError(error.response.data);
+    }
+
+    return Promise.reject(error);
+  },
+);
