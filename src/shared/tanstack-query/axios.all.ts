a0ad64a5d29654794f98041ba101b99abadf4194
aken-you <kinsk2839@naver.com>
2025-08-07 01:45:27 +0900

    refactor: axiosAllInstance의 withCredentials 제거

--- src/shared/tanstack-query/axios.all.ts
+++ src/shared/tanstack-query/axios.all.ts
@@ -8,7 +8,6 @@ export const axiosAllInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true,
 });
 
 axiosAllInstance.interceptors.response.use(
