35fb30eeab4f5234937fff2187575cff7fab91a0
aken-you <kinsk2839@naver.com>
2025-08-07 01:56:00 +0900

    refactor: access token 갱신 함수 삭제

--- src/shared/tanstack-query/get-new-access-token.ts
+++
@@ -1,9 +0,0 @@
-import { axiosAllInstance } from './axios.all';
-
-export const getNewAccessToken = async (): Promise<{
-  accessToken: string;
-}> => {
-  const res = await axiosAllInstance.get('/auth/refresh');
-
-  return res.data.content;
-};
