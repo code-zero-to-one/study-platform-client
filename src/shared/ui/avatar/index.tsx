97cdf8b8b749b850245fc7bea7c37d6fd52eca7e
조민주 <pida6716@gmail.com>
2025-08-21 00:44:16 +0900

    refactor: 색상 변경 가능하게 svgr로 변경

--- src/shared/ui/avatar/index.tsx
+++ src/shared/ui/avatar/index.tsx
@@ -1,26 +1,33 @@
 'use client';
 
-import { Avatar, AvatarImage } from '@/shared/shadcn/ui/avatar';
+import ProfileDefault from '@/entities/user/ui/icon/profile-default.svg';
+import { Avatar, AvatarImage, AvatarFallback } from '@/shared/shadcn/ui/avatar';
 
 interface UserAvatarProps {
   image?: string;
   alt?: string;
   size?: number;
+  accentColor?: string;
 }
 
 export default function UserAvatar({
   image,
   alt = 'user profile',
   size = 32,
-  ref,
+  accentColor = '#FAB0D5',
   ...props
-}: React.RefAttributes<HTMLSpanElement> & UserAvatarProps) {
+}: UserAvatarProps) {
   return (
-    <Avatar {...props} ref={ref} style={{ width: size, height: size }}>
+    <Avatar {...props} style={{ width: size, height: size }}>
       {image ? (
         <AvatarImage src={image} alt={alt} />
       ) : (
-        <AvatarImage src={'/profile-default.svg'} alt={alt} />
+        <AvatarFallback asChild>
+          <ProfileDefault
+            style={{ color: accentColor, width: size, height: size }}
+            aria-label={alt}
+          />
+        </AvatarFallback>
       )}
     </Avatar>
   );
