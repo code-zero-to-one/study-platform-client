3e63ea015e5689373fe36823f1d5b0568bc107b1
aken-you <kinsk2839@naver.com>
2025-08-23 17:23:19 +0900

    feat: 스터디 후기 추가되면, 안내 메세지 제공

--- src/features/study/model/use-review-query.ts
+++ src/features/study/model/use-review-query.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { addStudyReview, getPartnerStudyReview } from '../api/get-review';
 
 export const usePartnerStudyReviewQuery = () => {
@@ -11,5 +11,9 @@ export const usePartnerStudyReviewQuery = () => {
 export const useAddStudyReviewMutation = () => {
   return useMutation({
     mutationFn: addStudyReview,
+    onSuccess: () => {
+      // todo: 모달로 변경
+      alert('후기 작성이 완료되었습니다.');
+    },
   });
 };
