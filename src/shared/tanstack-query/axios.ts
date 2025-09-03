9aad95f57fff593160e1212b78e35bd27cf4ec15
aken-you <kinsk2839@naver.com>
2025-09-04 08:57:25 +0900

    feat: access token 재갱신

--- src/shared/tanstack-query/axios.ts
+++ src/shared/tanstack-query/axios.ts
@@ -1,8 +1,8 @@
 import axios, { InternalAxiosRequestConfig, isAxiosError } from 'axios';
-import { isApiError } from './api-error';
+import { ApiError, isApiError } from './api-error';
 import { getCookie, setCookie } from './cookie';
 
-// * 인증이 필요한 client-side axios 인스턴스
+// * client-side axios 인스턴스
 
 // json 요청
 export const axiosInstance = axios.create({
@@ -35,15 +35,6 @@ const onRequestClient = (config: InternalAxiosRequestConfig) => {
 
 axiosInstance.interceptors.request.use(onRequestClient);
 axiosInstanceForMultipart.interceptors.request.use(onRequestClient);
-
-const ERROR_MESSAGES = {
-  MEM001: '유효하지 않은 입력입니다.',
-  MEM002: '회원 정보가 존재하지 않습니다.',
-  MEM003: '이미 가입된 회원입니다.',
-  MEM004: '아직 스터디를 신청하지 않았습니다.',
-  MPR001: '관심사가 중복됐습니다.',
-  MPF001: '현재 프로젝트 에서 지원 하는 기능이 아닙니다.',
-};
 
 // refresh token을 사용해서 access token을 재갱신하는 함수
 const refreshAccessToken = async (): Promise<string | null> => {
@@ -154,8 +145,7 @@ axiosInstance.interceptors.response.use(
         }
       }
 
-      if (errorResponseBody.errorCode in ERROR_MESSAGES) {
-      }
+      return Promise.reject(new ApiError(errorResponseBody));
     }
 
     return Promise.reject(error);
@@ -177,10 +167,54 @@ axiosInstanceForMultipart.interceptors.response.use(
 
       // 유효하지 않은 accessToken인 경우, 재발급
       if (errorResponseBody.errorCode === 'AUTH001') {
+        if (isRefreshing) {
+          // 이미 토큰 갱신 중이면 대기열에 추가
+          return new Promise((resolve, reject) => {
+            failedQueue.push({ resolve, reject });
+          })
+            .then((token) => {
+              if (originalRequest) {
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+
+                return axiosInstance(originalRequest);
+              }
+            })
+            .catch((err) => {
+              return Promise.reject(err);
+            });
+        }
+
+        isRefreshing = true;
+
+        try {
+          const newAccessToken = await refreshAccessToken();
+
+          if (newAccessToken) {
+            processFailedQueue(null, newAccessToken);
+
+            if (originalRequest) {
+              originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+              return axiosInstance(originalRequest);
+            }
+          } else {
+            processFailedQueue(new Error('토큰 갱신 실패'), null);
+            window.location.href = '/login';
+
+            return Promise.reject(error);
+          }
+        } catch (refreshError) {
+          processFailedQueue(refreshError, null);
+          window.location.href = '/login';
+
+          return Promise.reject(refreshError);
+        } finally {
+          // eslint-disable-next-line require-atomic-updates
+          isRefreshing = false;
+        }
       }
 
-      if (errorResponseBody.errorCode in ERROR_MESSAGES) {
-      }
+      return Promise.reject(new ApiError(errorResponseBody));
     }
 
     return Promise.reject(error);
