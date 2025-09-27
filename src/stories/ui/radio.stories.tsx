f3d09fa8cc45babdc8fa5b94a0054e3686a13ade
aken-you <kinsk2839@naver.com>
2025-09-28 01:53:22 +0900

    feat: Radio 컴포넌트 스토리북 생성

---
+++ src/stories/ui/radio.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+import Button from '@/shared/ui/button';
+import { RadioGroup, RadioGroupItem } from '@/shared/ui/radio';
+
+const meta: Meta<typeof RadioGroup> = {
+  component: RadioGroup,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof RadioGroup>;
+
+export const Default: Story = {
+  render: () => {
+    const [state, setState] = useState('default');
+
+    return (
+      <div>
+        <RadioGroup className="flex flex-col gap-2.5" defaultValue="default">
+          <div className="flex items-center gap-100">
+            <RadioGroupItem
+              value="first"
+              id="option1"
+              onClick={() => setState('first')}
+            />
+            <label htmlFor="option1">first</label>
+          </div>
+          <div className="flex items-center gap-100">
+            <RadioGroupItem
+              value="second"
+              id="option2"
+              onClick={() => setState('second')}
+            />
+
+            <label htmlFor="option2">second</label>
+          </div>
+        </RadioGroup>
+
+        <Button type="button" onClick={() => console.log(state)}>
+          submit
+        </Button>
+      </div>
+    );
+  },
+};
