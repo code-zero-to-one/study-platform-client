6376a90f1f295e59f91b7bc814d183aa33d2dddd
Mimiminz <pida6716@gmail.com>
2025-09-28 03:13:42 +0900

    feat: 스텝 3 추가

---
+++ src/features/study/group/ui/step/step3-group.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import FormField from '@/shared/ui/form/form-field';
+import { TextAreaInput } from '@/shared/ui/input';
+import { OpenGroupFormValues } from '../../model/open-group-form.schema';
+
+export default function Step3GroupStudy() {
+  // todo: 그룹 폼 필드 변경해야 함
+  return (
+    <>
+      <div className="font-designer-20b text-text-default">
+        지원 & 규칙 설정
+      </div>
+      <FormField<OpenGroupFormValues, 'targetRole'>
+        name="targetRole"
+        label="스터디원에게 보여줄 질문을 입력하세요"
+        helper="스터디 지원자가 신청 시 작성해야 할 질문을 설정하세요. (예: 지원 동기, 경험, 기대하는 점 등)"
+        direction="vertical"
+        required
+      >
+        <TextAreaInput placeholder="지원동기" maxLength={500} />
+      </FormField>
+
+      <div className="rounded-150 bg-background-alternative flex flex-col gap-300 px-400 py-300">
+        <div className="font-designer-20b text-text-default">
+          스터디 리더님, 개설 규칙을 준수해주세요.
+        </div>
+        <ul className="font-designer-16m text-text-subtle list-inside list-disc space-y-100">
+          <li>모집 공고의 정보는 사실에 기반해 작성해야 합니다.</li>
+          <li>진행 기간과 모임 빈도를 명확히 안내해야 합니다.</li>
+          <li>스터디 개설 후 최소 1회 이상 정기 모임을 진행해야 합니다.</li>
+          <li>스터디원과의 약속을 존중하고 성실히 운영해야 합니다.</li>
+          <li>
+            타인의 개인정보 및 자료를 무단으로 공유하거나 외부 유출해서는 안
+            됩니다.
+          </li>
+          <li>플랫폼의 운영 정책 및 커뮤니티 가이드를 준수해야 합니다.</li>
+        </ul>
+      </div>
+    </>
+  );
+}
