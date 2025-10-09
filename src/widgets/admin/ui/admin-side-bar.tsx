8127979091bc340084eca6424fece07e533e3a6f
aken-you <kinsk2839@naver.com>
2025-10-10 00:38:25 +0900

    refactor: 어드민 사이드바 아이콘 수정

--- src/widgets/admin/ui/admin-side-bar.tsx
+++ src/widgets/admin/ui/admin-side-bar.tsx
@@ -1,35 +1,25 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import TabMenu from '@/shared/ui/tab-menu';
+import LogoutIcon from 'public/icons/logout.svg';
 
 export default function AdminSideBar() {
   return (
     <aside className="border-border-subtle h-screen w-fit border-r p-200">
       <div className="border-border-subtle flex items-center gap-150 border-b py-200">
-        <Image
-          src="icons/book.svg"
-          width={40}
-          height={40}
-          alt="admin-image"
-          className="rounded-full"
-        />
+        {/* 사용자 프로필 이미지 */}
 
         <div className="w-[136px]">
           <p className="font-designer-14m text-text-default">관리자</p>
           <p className="font-designer-12r text-text-subtle">kimkim@gmail.com</p>
         </div>
 
-        <Image src="icons/logout.svg" width={16} height={16} alt="logout" />
+        <LogoutIcon />
       </div>
 
       <nav className="mt-200">
         <TabMenu active={true}>
-          <Link
-            href="/admin"
-            className="bg-background-accent-blue-strong text-text-inverse font-designer-14m rounded-100 inline-block w-full px-200 py-150"
-          >
-            사용자 관리
-          </Link>
+          <Link href="/admin">사용자 관리</Link>
         </TabMenu>
       </nav>
     </aside>
