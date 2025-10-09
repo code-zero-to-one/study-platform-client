140106fadd40119aacdd4e816675921f5e923b81
aken-you <kinsk2839@naver.com>
2025-10-10 01:09:33 +0900

    feat: 어드민 사용자 상세 정보 페이지 layout 설정

---
+++ src/widgets/admin/ui/admin-detail-side-bar.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import Link from 'next/link';
+
+import { usePathname } from 'next/navigation';
+import TabMenu from '@/shared/ui/tab-menu';
+
+interface AdminDetailSideBarProps {
+  memberId: string;
+}
+
+export default function AdminDetailSideBar({
+  memberId,
+}: AdminDetailSideBarProps) {
+  const pathname = usePathname();
+
+  return (
+    <nav className="border-border-default rounded-100 flex h-fit w-[188px] flex-col border p-200">
+      <Link href={`/admin/detail/${memberId}/profile`}>
+        <TabMenu active={pathname === `/admin/detail/${memberId}/profile`}>
+          프로필
+        </TabMenu>
+      </Link>
+
+      <Link href={`/admin/detail/${memberId}/account-history`}>
+        <TabMenu
+          active={pathname === `/admin/detail/${memberId}/account-history`}
+        >
+          계정 이력
+        </TabMenu>
+      </Link>
+
+      <Link href={`/admin/detail/${memberId}/study`}>
+        <TabMenu active={pathname === `/admin/detail/${memberId}/study`}>
+          스터디
+        </TabMenu>
+      </Link>
+
+      <Link href={`/admin/detail/${memberId}/diligence`}>
+        <TabMenu active={pathname === `/admin/detail/${memberId}/diligence`}>
+          성실온도
+        </TabMenu>
+      </Link>
+    </nav>
+  );
+}
