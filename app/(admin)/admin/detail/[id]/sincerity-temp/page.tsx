e5c44a910760fdc1b3b4fdc25f463dee6cb8a36b
aken-you <kinsk2839@naver.com>
2025-10-11 02:52:57 +0900

    feat: 성실온도 내역 페이지 생성

--- app/(admin)/admin/detail/[id]/sincerity-temp/page.tsx
+++ app/(admin)/admin/detail/[id]/sincerity-temp/page.tsx
@@ -1,3 +1,54 @@
-export default function SincerityTempPage() {
-  return <div>SincerityTempPage</div>;
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { getSincerityTemperatureHistoryInServer } from '@/features/admin/api/sincerity-temperature-history.server';
+import { GetSincerityTemperatureHistoryResponse } from '@/features/admin/api/types';
+import SincerityTempTable from '@/features/admin/ui/sincerity-temp-table';
+
+export default async function SincerityTempPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const queryClient = new QueryClient();
+  const { id } = await params;
+
+  const memberId = Number(id);
+
+  // 서버 side에서 첫 페이지 데이터 미리 가져오기
+  await queryClient.prefetchQuery({
+    queryKey: ['sincerityTemperatureHistory', memberId, 1], // "sincerityTemperatureHistory", memberId, page
+    queryFn: () =>
+      getSincerityTemperatureHistoryInServer({
+        memberId: Number(memberId),
+        page: 1,
+      }),
+  });
+
+  const data: GetSincerityTemperatureHistoryResponse =
+    await queryClient.getQueryData([
+      'sincerityTemperatureHistory',
+      memberId,
+      1,
+    ]);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-col gap-300">
+        <div className="border-border-default rounded-100 flex w-full flex-col gap-200 border p-200">
+          <span className="font-designer-16b text-text-default">
+            현재 성실 온도
+          </span>
+
+          <div className="font-designer-14r text-text-subtle">
+            {data.currentSincerityTemperature} ℃
+          </div>
+        </div>
+
+        <SincerityTempTable memberId={Number(memberId)} />
+      </div>
+    </HydrationBoundary>
+  );
 }
--- src/features/admin/api/types.ts
+++ src/features/admin/api/types.ts
@@ -2,6 +2,7 @@ export type RoleId = 'ROLE_MEMBER' | 'ROLE_ADMIN' | 'ROLE_MENTOR';
 type RoleName = '일반' | '관리자' | '멘토';
 export type MemberStatus = 'ACTIVE' | 'PAUSED' | 'PERM_BAN' | 'DORMANT';
 
+// 사용자 리스트 조회 API 요청 타입
 export interface GetMemberListRequest {
   roleId?: RoleId;
   memberStatus?: MemberStatus;
@@ -9,6 +10,7 @@ export interface GetMemberListRequest {
   page?: number;
 }
 
+// 사용자 리스트 조회 API 응답 타입
 export interface GetMemberListResponse {
   page: number;
   size: number;
@@ -29,10 +31,12 @@ export interface GetMemberListResponse {
   }[];
 }
 
+// 사용자 계정 이력 조회 API 요청 타입
 export interface GetAccountHistoriesRequest {
   memberId: number;
 }
 
+// 사용자 계정 이력 조회 API 응답 타입
 export interface GetAccountHistoriesResponse {
   memberId: number;
   joinedAt: string;
@@ -50,12 +54,38 @@ export interface GetAccountHistoriesResponse {
   }[];
 }
 
+// 사용자 상태 변경 API 요청 타입
 export interface ChangeMemberStatusRequest {
   memberId: number;
   to: MemberStatus;
 }
 
+// 사용자 권한 변경 API 요청 타입
 export interface ChangeMemberRoleRequest {
   memberId: number;
   roleId: RoleId;
+}
+
+// 성실 온도 이력 조회 API 요청 타입
+export interface GetSincerityTemperatureHistoryRequest {
+  memberId: number;
+  page: number;
+}
+
+// 성실 온도 이력 조회 API 응답 타입
+export interface GetSincerityTemperatureHistoryResponse {
+  currentSincerityTemperature: number;
+  sincerityTemperatureHistory: {
+    content: {
+      reasonType: 'STUDY_REVIEW';
+      increment: number;
+      recordedAt: string;
+    }[];
+    page: number;
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    hasNext: boolean;
+    hasPrevious: boolean;
+  };
 }
