4edcf5952a3f0e3f1d19f47d93c106f7e4ed0da1
Mimiminz <pida6716@gmail.com>
2025-10-10 22:57:26 +0900

    style: 주석 변경

--- src/widgets/home/sidebar.tsx
+++ src/widgets/home/sidebar.tsx
@@ -61,7 +61,7 @@ export default async function Sidebar() {
         />
       )}
       {/* todo: 편하게 확인하시라고 추가했습니다. 이후 목록 추가되고 연결되면 제가 지워서 커밋 올리도록 하겠습니다. */}
-      <OpenGroupStudyModal
+      {/* <OpenGroupStudyModal
         trigger={
           <button className="bg-background-alternative rounded-100 flex items-center justify-between px-250 py-300">
             <p className="flex flex-col items-start gap-50">
@@ -74,7 +74,7 @@ export default async function Sidebar() {
             </p>
           </button>
         }
-      />
+      /> */}
       <Link
         href={''}
         className="bg-background-alternative rounded-100 flex items-center justify-between px-250 py-300"
