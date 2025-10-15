fdc00dea2e15ce16d880a2e30a743e516dbffe20
usernamebuzz <ohminji@ewhain.net>
2025-10-15 16:11:10 +0900

    chore: Apply prettier

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,4 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -28,5 +28,5 @@ export default function robots(): MetadataRoute.Robots {
       },
     ],
     sitemap: 'https://www.zeroone.it.kr/sitemap.xml',
-  }
+  };
 }
