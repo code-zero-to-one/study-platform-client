a47d164e0ccc24445639766ee64b3e2539eace51
aken-you <kinsk2839@naver.com>
2025-10-23 01:03:37 +0900

    style: 그룹 스터디 참가자들 query 커스텀 훅 네이밍 변경

--- src/features/study/group/model/use-group-study-member-list-query.ts
+++ src/features/study/group/model/use-group-study-member-list-query.ts
@@ -2,13 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 import { getGroupStudyMemberList } from '../api/get-group-study-member-list';
 import { GroupStudyMembersRequest } from '../api/group-study-types';
 
-export const useGroupStudyMembersQuery = ({
+export const useGroupStudyMemberListQuery = ({
   id,
   pageNumber,
   pageSize,
 }: GroupStudyMembersRequest) => {
   return useQuery({
-    queryKey: ['groupStudyMembers', id, pageNumber, pageSize],
+    queryKey: ['groupStudyMemberList', id, pageNumber, pageSize],
     queryFn: () => getGroupStudyMemberList({ id, pageNumber, pageSize }),
     enabled: !!id,
   });
