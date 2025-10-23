34b2cddb8ec000385e3cd190067278b30e21c20b
yeun38 <choh1997@naver.com>
2025-10-23 10:49:55 +0900

    feat: tabs type추가

--- src/features/study/group/ui/group-study-detail-page.tsx
+++ src/features/study/group/ui/group-study-detail-page.tsx
@@ -7,8 +7,10 @@ import Tabs from '@/shared/ui/tabs';
 import StudyInfoSection from './study-info-section';
 import { useGroupStudyDetailQuery } from '../model/use-study-query';
 
+type ActiveTab = 'intro' | 'members' | 'channel';
+
 export default function StudyDetailPage({ id: groupStudyId }: { id: number }) {
-  const [active, setActive] = useState('intro');
+  const [active, setActive] = useState<ActiveTab>('intro');
 
   const tabs = [
     { label: '스터디 소개', value: 'intro' },
@@ -41,7 +43,11 @@ export default function StudyDetailPage({ id: groupStudyId }: { id: number }) {
       </div>
 
       {/** 탭리스트 */}
-      <Tabs tabs={tabs} activeTab={active} onChange={setActive} />
+      <Tabs
+        tabs={tabs}
+        activeTab={active}
+        onChange={(value: ActiveTab) => setActive(value)}
+      />
       {active === 'intro' && (
         <StudyInfoSection study={studyDetail!} groupStudyId={groupStudyId} />
       )}
