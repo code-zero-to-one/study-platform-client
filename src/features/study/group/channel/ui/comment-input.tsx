82e3bb9d408f8cbd17f1eabead693ed5f99febf0
yeun38 <choh1997@naver.com>
2025-10-26 03:25:48 +0900

    feat:input ui 수정

--- src/features/study/group/channel/ui/comment-input.tsx
+++ src/features/study/group/channel/ui/comment-input.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useUser } from '@/features/auth/model/use-user';
 
 export default function CommentInput() {
   const { userId, userName } = useUser();
 
+  const [text, setText] = useState('');
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className="rounded-150 border-border-default flex flex-col gap-150 border-[1px] p-300">
+    <div
+      className={`rounded-150 border-border-default flex flex-col gap-150 border-[1px] p-300 ${isActive && 'border-border-strong'}`}
+    >
       <div className="flex justify-between">
         <span>{userName}</span>
-        <span>0/500</span>
+        <span>{text.length}/1000</span>
       </div>
 
-      <input />
+      <input
+        value={text}
+        placeholder="새로운 토론을 시작하세요."
+        onFocus={() => setIsActive(true)}
+        onBlur={() => setIsActive(false)}
+        className="rounded outline-none focus:outline-none"
+      />
 
       <div className="mt-150 flex justify-end gap-100">
         <button>취소</button>
