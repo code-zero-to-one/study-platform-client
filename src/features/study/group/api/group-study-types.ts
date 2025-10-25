6b1d628cb7e66e3f45b2868da2e74134da7ac106
aken-you <kinsk2839@naver.com>
2025-10-25 18:36:16 +0900

    refactor: 그룹 스터디 신청 상태에 EXIT 추가

--- src/features/study/group/api/group-study-types.ts
+++ src/features/study/group/api/group-study-types.ts
@@ -8,7 +8,12 @@ import {
 } from '../const/group-study-const';
 
 // 그룹 스터디 신청 상태
-type ApplicationStatus = 'PENDING' | 'APPROVED' | 'REJECTED' | 'KICKED';
+export type ApplicationStatus =
+  | 'PENDING' // 승인 대기
+  | 'APPROVED' // 승인 수락
+  | 'REJECTED' // 승인 거절
+  | 'KICKED' // 강퇴
+  | 'EXIT'; // 탈퇴
 export type GroupStudyStatus = 'RECRUITING' | 'IN_PROGRESS' | 'COMPLETED';
 export type GroupStudyType = 'PROJECT' | 'STUDY';
 export type HostType = 'ZEROONE' | 'GENERAL' | 'METOR';
