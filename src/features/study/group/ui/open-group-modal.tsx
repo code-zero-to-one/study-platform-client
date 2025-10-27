9fb1e7d09ccb7f1fbd3f67e4a230811cb22f4cb4
aken-you <kinsk2839@naver.com>
2025-10-27 15:06:24 +0900

    feat: 그룹스터디 상세 페이지에 스터디 개설 모달 연결

--- src/features/study/group/ui/open-group-modal.tsx
+++ src/features/study/group/ui/open-group-modal.tsx
@@ -17,7 +17,6 @@ import {
 import Step1OpenGroupStudy from './step/step1-group';
 import Step2OpenGroupStudy from './step/step2-group';
 import Step3OpenGroupStudy from './step/step3-group';
-import { EXTENSION_TO_MIME } from '../api/group-study-types';
 import { useCreateGroupStudyMutation } from '../const/use-group-study-mutation';
 
 function Stepper({ step }: { step: 1 | 2 | 3 }) {
@@ -56,11 +55,11 @@ interface OpenGroupStudyModalProps {
 
 export default function OpenGroupStudyModal({
   trigger,
-  open,
-  onOpenChange,
 }: OpenGroupStudyModalProps) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Modal.Root open={open} onOpenChange={onOpenChange}>
+    <Modal.Root open={open} onOpenChange={setOpen}>
       {trigger && <Modal.Trigger asChild>{trigger}</Modal.Trigger>}
       <Modal.Portal>
         <Modal.Overlay />
@@ -73,7 +72,7 @@ export default function OpenGroupStudyModal({
               <XIcon />
             </Modal.Close>
           </Modal.Header>
-          <OpenGroupStudyForm onClose={() => onOpenChange?.(false)} />
+          <OpenGroupStudyForm onClose={() => setOpen(false)} />
         </Modal.Content>
       </Modal.Portal>
     </Modal.Root>
