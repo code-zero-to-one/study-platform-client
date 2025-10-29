f958cc33d1955c1b41da73f26078142df5942bca
aken-you <kinsk2839@naver.com>
2025-10-29 20:32:05 +0900

    fix: 랜딩페이지 Layout 메인 컨텐츠 수정

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -41,10 +41,10 @@ export default function ServiceLayout({
         <MainProvider>
           <div className="w-full overflow-auto">
             {/** 1400 + 48*2 패딩 양옆 48로 임의적용 */}
-            <div className="m-auto flex w-[1496px] flex-1 flex-col items-center">
+            <div className="m-auto flex min-w-[1496px] flex-1 flex-col items-center">
               <Header />
+              <main className="w-full">{children}</main>
             </div>
-            <main className="w-full">{children}</main>
           </div>
         </MainProvider>
       </body>
