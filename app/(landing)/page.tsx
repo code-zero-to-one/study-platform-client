4ccc0d663b62a98a4ee006da3e686121b6800cb2
aken-you <kinsk2839@naver.com>
2025-10-29 19:41:11 +0900

    feat: 제로원 솔루션 정보 소개 섹션 구현

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import LoginModal from '@/features/auth/ui/login-modal';
+import Badge from '@/shared/ui/badge';
 import Button from '@/shared/ui/button';
 import LandingForm from '@/widgets/landing/form';
 
@@ -9,6 +10,33 @@ export const metadata: Metadata = {
   title: 'ZERO-ONE',
   description: '매일 아침을 함께 시작하는 1:1 기상 스터디 플랫폼, ZERO-ONE',
 };
+
+const SOLUTION_INFO_LIST = [
+  {
+    title: '빠른 시간 안에 성장할 수 있게',
+    description: [
+      '시간이 지날수록 늘어지는 취업 준비 기간을',
+      '단축시킬 수 있도록 도와드릴게요.',
+    ],
+    imageSrc: '/images/clock-in-landing-page.svg',
+  },
+  {
+    title: '원하는 만큼 배울 수 있도록',
+    description: [
+      '혼자서 독학으로 공부하기엔 버거웠던 시간을',
+      '자기계발과 함께 바꿔나갈 수 있도록 만들어드려요.',
+    ],
+    imageSrc: '/images/book-in-landing-page.svg',
+  },
+  {
+    title: 'A to Z STEP',
+    description: [
+      '한 걸음씩 단계를 밟아가며 개인 스터디부터',
+      '외주 프로젝트 경험을 쌓고, 채용 제안까지 연결해드려요.',
+    ],
+    imageSrc: '/images/shoes-in-landing-page.svg',
+  },
+];
 
 export default async function Landing() {
   return (
@@ -61,8 +89,48 @@ export default async function Landing() {
         </section>
 
         {/* 제로원 솔루션 소개 */}
+        <section className="flex flex-col items-center gap-500 py-[120px]">
+          <Badge className="rounded-200 w-fit">제로원 솔루션</Badge>
 
-        <section />
+          <p className="flex flex-col items-center gap-100">
+            <span className="font-bold-h2 text-text-strong">
+              중고 신입만 찾는 상황에서 어떻게 계속 준비할지 막막하신가요?
+            </span>
+            <span className="text-text-subtle font-designer-16m">
+              경력보다 실제 역량을 증명하고 그에 걸맞는 최고의 기회를 제안받을
+              수 있도록 제로원에서 해결해드릴게요.
+            </span>
+          </p>
+
+          <ul className="grid grid-cols-3 gap-300">
+            {SOLUTION_INFO_LIST.map((solution, index) => (
+              <li
+                key={index}
+                className="rounded-300 bg-background-default flex flex-col gap-250 px-400 py-500 shadow-[0_8px_30px_rgb(0,0,0,0.12)]"
+              >
+                <div className="flex flex-col items-center gap-200">
+                  <span className="font-bold-h5 text-text-default">
+                    {solution.title}
+                  </span>
+
+                  <div className="font-designer-14m text-text-subtlest flex flex-col items-center">
+                    {solution.description.map((des) => (
+                      <span key={des}>{des}</span>
+                    ))}
+                  </div>
+                </div>
+
+                <Image
+                  src={solution.imageSrc}
+                  alt={solution.title}
+                  width={280}
+                  height={280}
+                />
+              </li>
+            ))}
+          </ul>
+        </section>
+
         {/* 현직 전문가 멘토링 */}
         {/* 그룹 스터디 */}
         {/* 성실 온도 시스템 */}
