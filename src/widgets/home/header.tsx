f41208bd974cabac634f30850369e4e1098142f8
aken-you <kinsk2839@naver.com>
2025-10-29 17:50:56 +0900

    refactor: Header 컴포넌트 border 삭제

--- src/widgets/home/header.tsx
+++ src/widgets/home/header.tsx
@@ -26,12 +26,7 @@ export default async function Header() {
     : undefined;
 
   return (
-    <header
-      className={clsx(
-        'w-full bg-white px-600 py-[11px] mix-blend-multiply',
-        !isLoggedIn && 'border-b border-[#E7E8EA]',
-      )}
-    >
+    <header className="w-full bg-white px-600 py-[11px] mix-blend-multiply">
       <div className="flex w-full items-center justify-between">
         <div className="flex items-center gap-[7.5px] px-[8px] py-[11px]">
           <Image src="/icons/logo.svg" alt="Logo" width={18} height={18} />
