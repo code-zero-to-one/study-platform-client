463289f41cb84843046c4fb26848921bf48d90b2
aken-you <kinsk2839@naver.com>
2025-10-30 22:59:32 +0900

    feat: 멤버 그룹 스터디 클릭하면 그룹스터디 상세 페이지로 이동

--- src/features/my-page/ui/my-study-info-card.tsx
+++ src/features/my-page/ui/my-study-info-card.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { IoMdPeople } from 'react-icons/io';
 import { LuDot } from 'react-icons/lu';
 import { MemberStudyItem } from '@/features/study/group/api/group-study-types';
@@ -26,52 +27,55 @@ export default function MyStudyInfoCard({
 
   return (
     <li className="flex w-full flex-col gap-100">
-      <div className="relative">
-        <Image
-          src={thumbnail?.resizedImages[0].resizedImageUrl}
-          alt={`${studyId}`}
-          className="rounded-100 h-[244px] w-[280px] object-cover"
-          width={244}
-          height={210}
-        />
-        {status === 'COMPLETED' && (
-          <div className="rounded-100 absolute inset-0 bg-[#000000] opacity-50" />
-        )}
-      </div>
-
-      {status === 'RECRUITING' && (
-        <div className="inline-block w-auto">
-          <Badge color="purple">시작 전</Badge>
+      <Link href={`/study/${studyId}`} className="flex w-full flex-col gap-100">
+        <div className="relative">
+          <Image
+            src={thumbnail?.resizedImages[0].resizedImageUrl}
+            alt={`${studyId}`}
+            className="rounded-100 h-[244px] w-[280px] object-cover"
+            width={244}
+            height={210}
+          />
+          {status === 'COMPLETED' && (
+            <div className="rounded-100 absolute inset-0 bg-[#000000] opacity-50" />
+          )}
         </div>
-      )}
-      {status === 'IN_PROGRESS' && (
-        <div className="inline-block w-auto">
-          <Badge color="blue">진행 중</Badge>
-        </div>
-      )}
 
-      <div className="flex flex-col gap-50">
-        <div className="font-designer-15b">{title}</div>
-
-        <div className="text-text-subtle flex flex-row items-center justify-start gap-50">
-          <div className="font-designer-14m">
-            {studyRole === 'LEADER' ? '스터디 리더' : '스터디원'}
+        {status === 'RECRUITING' && (
+          <div className="inline-block w-auto">
+            <Badge color="purple">시작 전</Badge>
           </div>
-          <div className="flex flex-row">
-            <IoMdPeople />
-            <LuDot />
-            <div className="font-designer-12m">
-              {participantsCount} / {maxMembersCount}
+        )}
+        {status === 'IN_PROGRESS' && (
+          <div className="inline-block w-auto">
+            <Badge color="blue">진행 중</Badge>
+          </div>
+        )}
+
+        <div className="flex flex-col gap-50">
+          <div className="font-designer-15b">{title}</div>
+
+          <div className="text-text-subtle flex flex-row items-center justify-start gap-50">
+            <div className="font-designer-14m">
+              {studyRole === 'LEADER' ? '스터디 리더' : '스터디원'}
+            </div>
+            <div className="flex flex-row">
+              <IoMdPeople />
+              <LuDot />
+              <div className="font-designer-12m">
+                {participantsCount} / {maxMembersCount}
+              </div>
+            </div>
+          </div>
+
+          <div className="font-designer-12m text-text-subtle">
+            <div>
+              {startDate} ~{endDate && ` ${endDate}`}
             </div>
           </div>
         </div>
+      </Link>
 
-        <div className="font-designer-12m text-text-subtle">
-          <div>
-            {startDate} ~{endDate && ` ${endDate}`}
-          </div>
-        </div>
-      </div>
       {(status === 'RECRUITING' || status === 'IN_PROGRESS') &&
         participantsCount < maxMembersCount &&
         studyRole === 'LEADER' && (
