f6daba0448b45ad94e7681fdc58566f19cf6b1a5
aken-you <kinsk2839@naver.com>
2025-10-30 21:46:58 +0900

    feat: 멤버 그룹스터디 전체보기 페이지 연결

--- app/(service)/(my)/my-study/completed/page.tsx
+++ app/(service)/(my)/my-study/completed/page.tsx
@@ -1,28 +1,23 @@
 'use client';
 
 import { Plus } from 'lucide-react';
-import Link from 'next/link';
 import React from 'react';
 import { MemberStudyItem } from '@/features/study/group/api/group-study-types';
 import { useMemberStudyListQuery } from '@/features/study/group/model/use-member-study-list-query';
 import CompletedGroupStudyList from '@/features/study/group/ui/completed-group-study-list';
-import NotCompletedGroupStudyList from '@/features/study/group/ui/not-completed-group-study-list';
 import Button from '@/shared/ui/button';
 
 interface MemberGroupStudyList extends MemberStudyItem {
   type: 'GROUP_STUDY';
+  status: 'COMPLETED';
 }
 
-export default function MyStudy() {
+export default function CompletedPage() {
   const { data, isLoading } = useMemberStudyListQuery({
     memberId: 1,
     studyType: 'GROUP_STUDY',
-    studyStatus: 'BOTH',
+    studyStatus: 'COMPLETED',
   });
-
-  // status가 "IN_PROGRESS" 또는 "RECRUITMENT"인 스터디 목록
-  const notCompletedStudyList = (data?.notCompleted ||
-    []) as MemberGroupStudyList[];
 
   // status가 "COMPLETED"인 스터디 목록
   const completedStudyList = (data?.completed || []) as MemberGroupStudyList[];
@@ -44,42 +39,10 @@ export default function MyStudy() {
         <div>
           <div className="mb-200 flex flex-row justify-between">
             <h2 className="font-designer-16b text-text-default">
-              참여 중인 스터디
-            </h2>
-
-            {notCompletedStudyList.length > 9 && (
-              <Link
-                href="/my-study/not-completed"
-                className="font-designer-14m text-text-subtlest"
-              >
-                전체보기
-              </Link>
-            )}
-          </div>
-
-          <NotCompletedGroupStudyList
-            studyList={notCompletedStudyList.filter((study, idx) => idx < 9)}
-          />
-        </div>
-
-        <div>
-          <div className="mb-200 flex flex-row justify-between">
-            <h2 className="font-designer-16b text-text-default">
               종료된 스터디
             </h2>
-
-            {completedStudyList.length > 9 && (
-              <Link
-                href="/my-study/completed"
-                className="font-designer-14m text-text-subtlest"
-              >
-                전체보기
-              </Link>
-            )}
           </div>
-          <CompletedGroupStudyList
-            studyList={completedStudyList.filter((study, idx) => idx < 9)}
-          />
+          <CompletedGroupStudyList studyList={completedStudyList} />
         </div>
       </div>
     </div>
