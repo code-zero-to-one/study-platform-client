00a9bf989492f91d522f1af6a9f7b97b194dc6d7
aken-you <kinsk2839@naver.com>
2025-12-19 23:13:08 +0900

    feat: openapi instance 생성 함수

---
+++ src/api/client/open-api-instance.ts
@@ -0,0 +1,23 @@
+import { AxiosInstance } from 'axios';
+import { Configuration } from '../openapi';
+import { axiosInstance } from './axios';
+import { getCookie } from './cookie';
+
+// OpenAPI용 Configuration
+const openapiConfig = new Configuration({
+  basePath: process.env.NEXT_PUBLIC_API_BASE_URL,
+  accessToken: () => getCookie('accessToken') ?? undefined,
+});
+
+// API 인스턴스 생성 헬퍼
+// 대부분의 경우 axiosInstance를 사용함
+// axiosInstanceForMultipart가 필요한 경우는 별도 구현 필요
+export const createApiInstance = <T>(
+  ApiClass: new (
+    config: Configuration,
+    basePath?: string,
+    axios?: AxiosInstance,
+  ) => T,
+): T => {
+  return new ApiClass(openapiConfig, openapiConfig.basePath, axiosInstance);
+};
