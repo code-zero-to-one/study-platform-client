9fca14ea4ad8a35f4e310ca91cade4ef08abbd73
yeun38 <choh1997@naver.com>
2026-01-07 00:59:57 +0900

    환경 변수 테스트

--- src/components/summary/order-summary.tsx
+++ src/components/summary/order-summary.tsx
@@ -16,18 +16,13 @@ export default function OrderSummary({
   return (
     <div className="flex flex-col gap-300">
       <div className="flex items-center gap-250">
-        {/* {thumbnailUrl ? (
-          <Image
-            src={thumbnailUrl ?? ''}
-            alt={groupStudyTitle}
-            width={90}
-            height={60}
-            className="rounded-25 h-[60px] w-[90px] object-cover"
-          />
-        ) : (
-          <div className="rounded-25 h-[60px] w-[90px] bg-[#F97283]" />
-        )} */}
-        <div className="rounded-25 h-[60px] w-[90px] bg-[#F97283]" />
+        <Image
+          src={thumbnailUrl ?? ''}
+          alt={groupStudyTitle}
+          width={90}
+          height={60}
+          className="rounded-25 h-[60px] w-[90px] object-cover"
+        />
         <div className="flex flex-1 flex-row items-end justify-between">
           <div className="space-y-1">
             <p className="font-designer-16m">{groupStudyTitle}</p>
