dec628dad6d45f2996f3cd08759bac30e74f2680
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2026-02-19 14:58:56 +0900

    feat : 최소 길이 추가

--- src/features/study/group/model/question.schema.ts
+++ src/features/study/group/model/question.schema.ts
@@ -8,20 +8,28 @@ export enum QuestionCategory {
   CONCERN = 'CONCERN',
 }
 
+export const QUESTION_TITLE_MIN_LENGTH = 2;
+export const QUESTION_CONTENT_MIN_LENGTH = 20;
 export const QUESTION_TITLE_MAX_LENGTH = 255;
 export const QUESTION_CONTENT_MAX_LENGTH = 3000;
 
 export const questionSchema = z.object({
   title: z
     .string()
-    .min(1, '제목을 입력해주세요.')
+    .min(
+      QUESTION_TITLE_MIN_LENGTH,
+      `제목은 ${QUESTION_TITLE_MIN_LENGTH}자 이상 입력해주세요.`,
+    )
     .max(
       QUESTION_TITLE_MAX_LENGTH,
       `제목은 ${QUESTION_TITLE_MAX_LENGTH}자 이하로 입력해주세요.`,
     ),
   content: z
     .string()
-    .min(1, '내용을 입력해주세요.')
+    .min(
+      QUESTION_CONTENT_MIN_LENGTH,
+      `내용은 ${QUESTION_CONTENT_MIN_LENGTH}자 이상 입력해주세요.`,
+    )
     .max(
       QUESTION_CONTENT_MAX_LENGTH,
       `내용은 ${QUESTION_CONTENT_MAX_LENGTH}자 이하로 입력해주세요.`,
